4623af1e1f03edb9f84ae9317cbc69c5bd0f6899
vikaskashyap7 <vikaskashyap1334@gmail.com>
2024-01-23 22:09:22 +0530

    first

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div style={{marginTop: "150px", textAlign: "center"}}>
+        <h2>
+          {""}
+          Task Management System using React and Firebase
+        </h2>
+        <br />
+        
+        <a href="https://ishansandhu.ca"
+          target='_blank' rel="noreferrer">
+          Made by Ishan Sandhu 👨‍💻
+        </a>
+    </div>
+  )
+}
+
+export default About
